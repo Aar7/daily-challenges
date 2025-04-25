51ba5bd441d3c32d7512226c1033dcdd681bd420
AaronMeighoo <aaronmeighoo1@gmail.com>
2025-04-25 12:34:14 -0500

    scaffold main project parts

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return <footer className="footer">This is the footer</footer>;
+}
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,5 @@
+import "./Main.css";
+
+export default function Main() {
+  return <div className="main">This is the main part of the application</div>;
+}
